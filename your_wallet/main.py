9e669bc3188dff24d1173ccc94f5f121beefe088
Mark <marko4148@gmail.com>
2022-05-27 19:52:41 +0300

    Registered routers

--- your_wallet/main.py
+++ your_wallet/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 
+import config
 
-app = FastAPI()
+
+from routers import users
+from routers import currencies
+from routers import user_wallets
 
 
-@app.get('/')
-def root():
-	return {'Hello': 'World'}
-
+app = FastAPI(docs_url='/api/v1/docs/')
+app.include_router(users.router, prefix='/api/v1')
+app.include_router(currencies.router, prefix='/api/v1')
+app.include_router(user_wallets.router, prefix='/api/v1')
