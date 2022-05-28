46091a2fa13c902acd88bbe88f8137256718b969
Mark <marko4148@gmail.com>
2022-05-28 18:47:20 +0300

    Added unique constraint to some fields

--- your_wallet/database/models.py
+++ your_wallet/database/models.py
@@ -47,14 +47,14 @@ class TransactionType(WalletBase):
     __tablename__ = 'transaction_types'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(30), nullable=False)
+    name = Column(String(30), nullable=False, unique=True)
 
 
 class TransactionCategory(WalletBase):
     __tablename__ = 'transaction_categories'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False)
+    name = Column(String(50), nullable=False, unique=True)
 
 
 class Transaction(WalletBase):
