efb52f7c631ae3ed1bf04cd63c4ce719b1da6885
Mark <marko4148@gmail.com>
2022-05-28 19:12:47 +0300

    Registered routers

--- your_wallet/main.py
+++ your_wallet/main.py
@@ -6,9 +6,13 @@ import config
 from routers import users
 from routers import currencies
 from routers import user_wallets
+from routers.transactions import categories
+from routers.transactions import types as transactions_types
 
 
 app = FastAPI(docs_url='/api/v1/docs/')
 app.include_router(users.router, prefix='/api/v1')
 app.include_router(currencies.router, prefix='/api/v1')
 app.include_router(user_wallets.router, prefix='/api/v1')
+app.include_router(categories.router, prefix='/api/v1')
+app.include_router(transactions_types.router, prefix='/api/v1')
