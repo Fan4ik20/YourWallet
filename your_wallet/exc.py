bdab38774abaa6d317c1e8f712a1588ce211d7b4
Mark <marko4148@gmail.com>
2022-06-16 18:28:13 +0300

    Added exceptions and handlers

---
+++ your_wallet/exc.py
@@ -0,0 +1,9 @@
+class ObjectNotExist(Exception):
+    def __init__(self, model_name: str):
+        self.model = model_name
+
+
+class ObjectWithGivenAttrExist(Exception):
+    def __init__(self, model_name: str, attr_name: str):
+        self.model = model_name
+        self.attr = attr_name
--- your_wallet/main.py
+++ your_wallet/main.py
@@ -1,16 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 import config
+import exc
 
-from database.interfaces import DbInterface
+from database.interfaces.db_interface import DbInterface
 
 from routers import users
 from routers import currencies
 from routers import user_wallets
 from routers.transactions import categories
 from routers.transactions import wallet_transactions
-
 
 app = FastAPI(docs_url='/api/v1/docs/')
 DbInterface.create_tables()
@@ -33,3 +34,27 @@ app.include_router(currencies.router, prefix='/api/v1')
 app.include_router(user_wallets.router, prefix='/api/v1')
 app.include_router(categories.router, prefix='/api/v1')
 app.include_router(wallet_transactions.router, prefix='/api/v1')
+
+
+@app.exception_handler(exc.ObjectNotExist)
+def object_not_exist_handler(
+        request: Request, exc_: exc.ObjectNotExist
+) -> JSONResponse:
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={
+            'message': f'{exc_.model} with given identifier does not exist!'
+        }
+    )
+
+
+@app.exception_handler(exc.ObjectWithGivenAttrExist)
+def object_with_given_attr_exist_handler(
+        request: Request, exc_: exc.ObjectWithGivenAttrExist
+) -> JSONResponse:
+    return JSONResponse(
+        status_code=status.HTTP_409_CONFLICT,
+        content={
+            'message': f'{exc_.model} with given {exc_.attr} already exist'
+        }
+    )
