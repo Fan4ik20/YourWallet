30fea8b41279606216df760d146ca040d56789b9
Mark <marko4148@gmail.com>
2022-06-16 17:29:41 +0300

    Added Cors

--- your_wallet/main.py
+++ your_wallet/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import config
 
@@ -13,6 +14,19 @@ from routers.transactions import wallet_transactions
 
 app = FastAPI(docs_url='/api/v1/docs/')
 DbInterface.create_tables()
+
+origins = [
+    "http://127.0.0.1:5000",
+    "http://localhost:5000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router, prefix='/api/v1')
 app.include_router(currencies.router, prefix='/api/v1')
